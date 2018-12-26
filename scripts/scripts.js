26aa131e5d30d9e4f5ca17a47eb6fe20d24ae4b2
ignacio-cortes-externo <ignacio.cortes.externo@axa.com>
2018-12-26 12:39:59 +0100

    Nonrepeatable marker

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,7 +1,7 @@
 initMap();
 
 var id = 0;
-var markers = [];
+var markers;
 var infowindow;
 
 function initMap() {
@@ -32,34 +32,81 @@ function geocodeAddress(geocoder, resultsMap) {
 
         if (status === 'OK') {
 
-            resultsMap.setZoom(10);
+            if ( resultsMap.getZoom() != 10 ) {
+                resultsMap.setZoom(10);
+            }
+
             resultsMap.setCenter(results[0].geometry.location);
-            
-            var marker = new google.maps.Marker({
-                id: id,
-                map: resultsMap,
-                position: results[0].geometry.location,
-                title: 'Posición',
-                snippet: results[0].formatted_address,
-                animation: google.maps.Animation.DROP
-            }, id++);
 
-            markers.push(marker);
+            if (markers) {
 
-            marker.addListener('click', function() {
+                var repMarker = false;
 
-                infowindow = new google.maps.InfoWindow({
-                    content: '<div class="custom-iw"><p><b>' + this.title + '</b></p><p>' + this.snippet + '<p></div>',
+                for (let i = 0; i < markers.length; i++) {
+                    if ( markers[i].title === results[0].address_components[0].long_name ) {
+                        repMarker = true;
+                    }
+                }
+
+                if ( repMarker === true ) {
+
+                    alert('Marcador repetido');
+
+                } else {
+
+                    var marker = new google.maps.Marker({
+                        id: id,
+                        map: resultsMap,
+                        position: results[0].geometry.location,
+                        title: results[0].address_components[0].long_name,
+                        snippet: results[0].formatted_address,
+                        animation: google.maps.Animation.DROP
+                    }, id++);
+        
+                    markers.push(marker);
+        
+                    marker.addListener('click', function() {
+        
+                        infowindow = new google.maps.InfoWindow({
+                            content: '<div class="custom-iw"><p>' + this.snippet + '<p></div>',
+                        });
+        
+                        infowindow.open(map, this);
+        
+                        $('#remove-marker').addClass('active');
+                        $('#remove-marker').attr('data-id', marker.id);
+        
+                    });
+                }
+
+            } else {
+
+                var marker = new google.maps.Marker({
+                    id: id,
+                    map: resultsMap,
+                    position: results[0].geometry.location,
+                    title: results[0].address_components[0].long_name,
+                    snippet: results[0].formatted_address,
+                    animation: google.maps.Animation.DROP
+                }, id++);
+    
+                markers = [];
+                markers.push(marker);
+    
+                marker.addListener('click', function() {
+    
+                    infowindow = new google.maps.InfoWindow({
+                        content: '<div class="custom-iw"><p>' + this.snippet + '<p></div>',
+                    });
+    
+                    infowindow.open(map, this);
+    
+                    $('#remove-marker').addClass('active');
+                    $('#remove-marker').attr('data-id', marker.id);
+    
                 });
 
-                console.log(infowindow);
-
-                infowindow.open(map, this);
-
-                $('#remove-marker').addClass('active');
-                $('#remove-marker').attr('data-id', marker.id);
-
-            });
+            }
 
         } else {
             alert('Geocode was not successful for the following reason: ' + status);
@@ -77,6 +124,19 @@ function deleteMarker() {
         return obj.id === idd
     });
 
-    result[0].setMap(null);
+    markers.splice(result[0], 1); // Remove marker from array
+    result[0].setMap(null); // Remove marker from map
+
+    if (markers.length == 0) {
+        $('#remove-marker').removeClass('active');
+    }
+
+}
+
+function getPlaces() {
+
+    for (let i = 0; i < markers.length; i++) {
+        console.log(markers[i]);
+    }
 
 }
